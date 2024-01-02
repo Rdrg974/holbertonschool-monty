135eeae31ea9e425286d6c0bc9e31039c10a1524
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-02 07:21:37 -0800

    All the prototypes and the structures

---
+++ monty.h
@@ -0,0 +1,46 @@
+#ifndef __MONTY__H__
+#define __MONTY__H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+char **tokenize(char *line, int line_number);
+int convert_if_int(char *second_argument, int line_number);
+void push_function(int number);
+void *handle_opcode(stack_t **stack, unsigned int line_number);
+
+#endif
