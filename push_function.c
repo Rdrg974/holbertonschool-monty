b0300e10d5d23633de2f2a5651ef30a5224f1d18
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-03 05:21:47 -0800

    Added function to free memory

--- push_function.c
+++ push_function.c
@@ -50,14 +50,16 @@ void push_function(stack_t **stack, unsigned int number)
 	(*stack) = new_node;
 }
 
-/*void free_stack(stack_t **stack)
+void free_stack(stack_t **stack)
 {
-	stack_t **tmp;
+	stack_t **tmp = NULL;
 
+	if ((*tmp) == NULL)
+		return;
 	while ((*stack) != NULL)
 	{
 		(*tmp) = (*stack);
 		(*stack) = (*stack)->next;
 		free(tmp);
 	}
-}*/
+}
