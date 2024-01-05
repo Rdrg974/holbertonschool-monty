18d69e9d647d132369fd57fc24e8eed3e7dd0d5c
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-05 02:18:54 -0800

    Function that swaps two top elements of the stack

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * swap - swap the top two elements of the stack
+ * @stack: stack containing elements
+ * @line_number: line number
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp1;
+	int new = 0;
+
+	if (*stack == NULL || ((*stack)->prev == NULL && (*stack)->next == NULL))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	tmp1 = *stack;
+	new = (*stack)->n;
+	tmp1->n = (*stack)->next->n;
+	(*stack)->next->n = new;
+}
