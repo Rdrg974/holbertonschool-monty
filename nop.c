c67ae8c79ebe4f6b773093675db3b79669ef05a2
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-05 01:17:58 -0800

    Function that doesn't do anything

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything
+ * @stack: stack containing elements
+ * @line_number: number of lines
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
